3c3796a6d899380092d0d9d06102746a1cc6483a
ekip-s <F6900921f>
2022-11-06 19:40:08 +0300

    feat: добавлени базы данных и бронирования

--- src/main/java/ru/practicum/shareit/ShareItApp.java
+++ src/main/java/ru/practicum/shareit/ShareItApp.java
@@ -1,9 +1,8 @@
 package ru.practicum.shareit;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import ru.practicum.shareit.user.service.UserServiceJPA;
+
 
 @SpringBootApplication
 public class ShareItApp {
